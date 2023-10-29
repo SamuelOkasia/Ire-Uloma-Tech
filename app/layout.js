4ef9ca111901f909973f0d9b66d62e1b9d000472
samuel <samuelokasia1@gmail.com>
2023-10-29 20:37:47 +0000

    First commit

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import Nav from "../components/nav/Nav";
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+          <div className="container">
+              <Nav/>
+              <div className="container_components">
+                  {children}
+              </div>
+          </div>
+      </body>
     </html>
   )
 }
